b5776479b37b6fd34c6a95407eb2be7f9c4feb7b
suptokumar <personal.supto@gmail.com>
2021-03-07 18:44:15 +0600

    Simple Routing

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+<style>
+.full-height {
+    height: 96vh;
+    font: 35px arial;
+}
+
+.flex-center {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+}
+</style>
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <div style="text-align: center;">
+           <h1>To buy something you need to come again after a month.</h1>
+        </div>
+</div>
+</div>
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/buy', function () {
+    return view('hello');
+});
