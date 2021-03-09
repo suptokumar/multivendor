b849eb81127eb2fc9bbed1d56124637227668c9a
suptokumar <personal.supto@gmail.com>
2021-03-09 15:27:15 +0600

    Designed with Ecommerce Header and Laravel Migration

---
+++ database/migrations/2021_03_09_090236_create_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_id');
+            $table->string('product_name');
+            $table->string('product_brand');
+            $table->string('product_size');
+            $table->string('product_color');
+            $table->string('product_category');
+            $table->integer('product_price');
+            $table->integer('product_discount');
+            $table->integer('product_quantity');
+            $table->longtext('product_description');
+            $table->string('product_image');
+            $table->string('product_galary');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,37 +1,37 @@
 <style>
-ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  background-color: #333;
-}
-
-li {
-  float: left;
-}
-
-li a {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-
-li a:hover {
-  background-color: #111;
-}
-li a.{{$home}}{
-	background: green;
+.top_nav li a.{{$home}}{
+	background: #1e4478;
 }
 </style>
-</head>
-<body>
-
+<div class="top_nav">
+<div>
+	<a href="deals" href="deals">Best Deals</a>
+</div>
 <ul>
   <li><a class="home" href="{{url('/')}}">Home</a></li>
-  <li><a class="news" href="{{url('/news')}}">News</a></li>
+  <li><a class="news" href="{{url('/shop')}}">Shop</a></li>
   <li><a class="contact" href="{{url('/contact')}}">Contact</a></li>
-  <li><a class="about" href="{{url('/about')}}">About</a></li>
+  <li><a class="about" href="{{url('/about')}}">About us</a></li>
+  <li><a class="about" href="{{url('/login')}}">Login</a></li>
 </ul>
+</div>
+<div class="fulll_stak_menu">
+	<div class="partision">
+		
+<img src="{{asset('/img/onlineBD.PNG')}}" alt="onlineBD">
+	</div>
+	<div class="partision form">
+		
+<form action="{{url('/search/')}}" method="GET">
+	<input type="search" autocomplete="off"  placeholder="Search..." id="search" name="search"><input type="submit" name="submit" value="Search">
+	<div class="search_result"></div>
+</form>
+	</div>
+<div class="partision">
+<div class="user_setting">
+	<a href="{{url('/wishlist')}}"><span class="fa fa-heart"></span> <span class="wo">Wishlist</span></a>
+	<a href="{{url('/cart')}}"><span class="fa fa-cart-arrow-down"></span>  <span class="wo">My Cart</span></a>
+	<a href="{{url('/account')}}"><span class="fa fa-user-circle"></span> <span class="wo">My Account</span></a>
+</div>
+</div>
+</div>
